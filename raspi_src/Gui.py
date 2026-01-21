ae8498d1823b3d292d0264d81c4c6dbf5a4c33d3
Jack Chambers <jtcroxley@gmail.com>
2026-01-21 14:01:39 -0800

    gui changes

--- raspi_src/Gui.py
+++ raspi_src/Gui.py
@@ -5,3 +5,24 @@ Gui
 Jack
 
 """
+
+import tkinter
+
+class Gui:
+    def __init__(self):
+        self.root = tkinter.Tk()
+
+    def open(self) -> None:
+        # init stuff
+        self.root.title("Debug Gui")
+        self.root.geometry('320x200')
+        self._initGeometry()
+        self.root.mainloop()
+
+
+    def _initGeometry(self):
+        pass
+
+if __name__ == "__main__":
+    gui = Gui()
+    gui.open()
