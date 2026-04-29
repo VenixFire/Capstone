ae35d7f6fc039f421f26ccc0506b9c0e75c4f32d
Jack Chambers <jtcroxley@gmail.com>
2026-04-28 21:30:19 -0700

    remove creating that file, need to revisit the service

--- launch.sh
+++ launch.sh
@@ -14,7 +14,3 @@ echo -e "\n--// Launching avahi-daemon"
 
 ## Enable Access Point with network-manager
 echo -e "\n--// Enabling Access Point"
-
-FILE="/home/logger/created_by_service"
-touch $FILE
-echo "$(whoami)" > $FILE
