d08ca4fe0441c5a7de570d9a1dbe05e85b9b34f3
Jack Chambers <jtcroxley@gmail.com>
2026-04-28 17:55:23 -0700

    Update scripts

--- install.sh
+++ install.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# VandalOptics
+# install.sh
+# Contributors: Jack Chambers
+#
+# Install necessary dependencies and such for the program
+
 echo ---- VANDAL OPTICS INSTALLING DEPENDENCIES ----
 USER=$(whoami)
 ROOT=root
 
+AP_CONF=
+AP_NAME="VandalOptics"
+AP_PWD="GoVandals!"
+
+# /etc/udev/rules.d/...
+RULE_FILE="/etc/udev/rules.d/ThorLabs.rules"
+AP_CONF_FILE="/etc/NetworkManager/system-connections/AccessPoint.nmconnection"
+
 if [[ "$USER" == "$ROOT" ]]; then
 
+    # Install necessary python packages
+    echo
     echo "--// Installing Necessary Python Packages"
-    #
     sudo apt install python3-pyvisa python3-zeroconf python3-psutil
 
 
-
+    # Install necessary linux packages
+    echo
     echo "--// Installing Necessary Packages"
-    #
     sudo apt install avahi-daemon network-manager
 
 
-
-    # /etc/udev/rules.d
-    RULE_FILE="/etc/udev/rules.d/ThorLabs.rules"
+    # Define ruleset for pyvisa access to peripherals
+    echo
     echo "--// Adding rules for PyVisa Access"
     if [ -f "$RULE_FILE" ]; then
         echo "--// Rule file already exists, no overwriting"
@@ -28,6 +42,15 @@ if [[ "$USER" == "$ROOT" ]]; then
         echo -e "\#USBTMC Instruments\n\# ThorLabs PM61\nSUBSYSTEMS=="usb", ACTION=="add", ATTRS{idVendor}=="1313", ATTRS{idProduct}=="80b4", GROUP="usbtmc", MODE="0660"" > /etc/udev/rules.d/ThorLabs.rules
         echo "--// Successfully wrote rules"
     fi
+
+
+    # Check for existing network configuration
+    if $(nmcli conn | grep AccessPoint); then
+        echo "--// Access point config file already exists."
+    else
+        echo "--// Creating new AccessPoint connection"
+        sudo nmcli dev wifi hotspot ifname wlan0 ssid $AP_NAME password $AP_PWD con-name "AccessPoint"
+    
 
 else
     echo "--// You need to run as sudo -- 'sudo install.sh'"
--- launch.sh
+++ launch.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-echo TODO!
+
+# VandalOptics
+# launch.sh
+# Contributors: Jack Chambers
+# 
+# Launches necessary components / features for operation
+
+## Title
+echo "---- VANDAL OPTICS LAUNCH SCRIPT ----"
+
+## Start avahi-daemon for dns resolution
+echo -e "\n--// Launching avahi-daemon"
+
+## Enable Access Point with network-manager
+echo -e "\n--// Enabling Access Point"
